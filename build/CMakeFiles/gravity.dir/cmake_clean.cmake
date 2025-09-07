6f752b0920aab00974b776c19b4ac7af05280c91
Sanket Srivastava <sanketsriv22@gmail.com>
2025-09-07 16:27:43 -0400

    initialized repo, set up cmake

---
+++ build/CMakeFiles/gravity.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gravity.dir/main.cpp.o"
+  "CMakeFiles/gravity.dir/main.cpp.o.d"
+  "gravity"
+  "gravity.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gravity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Beginning an OpenGL project that simulates gravity." << std::endl;
+    return 0;
+}
