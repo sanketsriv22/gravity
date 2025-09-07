04df3e8e1f169c3763c5fd30287331f830aa60b7
Sanket Srivastava <sanketsriv22@gmail.com>
2025-09-07 19:23:53 -0400

    created single triangle using openGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
 
 // resizing callback
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -22,12 +39,14 @@ int main()
 {
     std::cout << "Beginning an OpenGL project that simulates gravity." << std::endl;
 
+    // init GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // create a window
     GLFWwindow* window = glfwCreateWindow(1280, 800, "Gravity Simulation", NULL, NULL);
     if (window==NULL) // returns null if doesn't work
     {
@@ -37,6 +56,7 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // create a callback for window resizing!
 
     // initialize GLAD?
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
@@ -45,12 +65,62 @@ int main()
         return -1;
     }
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // create a callback for window resizing!
     glViewport(0, 0, 1280, 800);
+
+    // trying to draw a triangle
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f, 0.5f, 0.0f 
+    };
+
+    // compile a vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // compile fragment shader - calculate color output of pixels
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Final linked version of multiple shaders combined
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // vertex array and buffer objects
+    GLuint VAO, VBO;
+
+    // create and bind VAO first
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
     
+    // create and setup VBO
+    glGenBuffers(1, &VBO); // passing reference so no copy made
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // configure vertex attributes (once)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
     while(!glfwWindowShouldClose(window))
     {
         processInput(window); // created to exit on esc key press
+
+        // draw
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window); // double buffer rendering
         glfwPollEvents();        // checks for any event triggering, updates window state and calls the right functions
