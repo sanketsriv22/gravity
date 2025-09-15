6114bce34ca8e781c8d16fcf751545906f0b1acf
Sanket Srivastava <sanketsriv22@gmail.com>
2025-09-15 17:53:36 -0400

    simple shaders for spacetime grid

--- utils/shaders.h
+++ utils/shaders.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// planet shaders
 const char *vertexShaderSource = 
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -16,7 +17,6 @@ const char *vertexShaderSource =
     "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
     "   fragPos = aPos.xy;\n"
     "}\0";
-
 
 const char *fragmentShaderSource = 
     "#version 330 core\n"
@@ -35,4 +35,26 @@ const char *fragmentShaderSource =
     "   normalizedDistance = clamp(normalizedDistance, 0.0, 1.0);\n"
     "   vec3 color = mix(centerColor, edgeColor, normalizedDistance);\n"
     "   FragColor = vec4(color, 1.0f);\n"
+    "}\0";
+
+
+// spacetime grid shaders
+const char *gridVertexShaderSource = 
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 projection;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = projection * view * vec4(aPos, 1.0);\n"
+    "}\0";
+
+
+const char *gridFragmentShaderSource = 
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "uniform vec3 uColor;"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(uColor, 1.0f);\n"
     "}\0";
